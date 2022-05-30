533eb69db10af1b121e46b83d94afdb8e81060ec
guilhermeR20 <guilherme.rochalima17@gmail.com>
2022-05-29 23:02:09 -0300

    Crud inicial completo

---
+++ app/src/main/java/com/agenda/agendatelefonica/Conexao.java
@@ -0,0 +1,29 @@
+package com.agenda.agendatelefonica;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Conexao extends SQLiteOpenHelper {
+
+    private static final String name = "banco.db";
+    private static final int version = 1;
+
+
+    public Conexao(@Nullable Context context) {
+        super(context, name, null, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table aluno(id integer primary key autoincrement," +
+                "nome varchar(50), cpf varchar(50), telefone varchar(50))") ;
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
